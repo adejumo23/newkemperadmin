902c52ed9cd38233c21a5477e888f38a7081493a
Prudhvi Raj Maraboyena <xanthusblack@gmail.com>
2020-01-21 23:43:47 -0600

    Added Zend framework modules

---
+++ config/development.config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+return [
+    // Additional modules to include when in development mode
+    'modules' => [
+        'Zend\Log',
+        'Zend\Db',
+        'Zend\ServiceManager\Di',
+        'Zend\Mvc\Plugin\FilePrg',
+        'Zend\Mvc\Plugin\FlashMessenger',
+        'Zend\Mvc\Plugin\Identity',
+        'Zend\Mvc\Plugin\Prg',
+        'Zend\Session',
+        'Zend\Mvc\Console',
+        'Zend\Form',
+        'Zend\Hydrator',
+        'Zend\InputFilter',
+        'Zend\Filter',
+        'Zend\I18n',
+        'Zend\Cache',
+        'ZendDeveloperTools',
+    ],
+    // Configuration overrides during development mode
+    'module_listener_options' => [
+        'config_glob_paths' => [realpath(__DIR__) . '/autoload/{,*.}{global,local}-development.php'],
+        'config_cache_enabled' => false,
+        'module_map_cache_enabled' => false,
+    ],
+];
